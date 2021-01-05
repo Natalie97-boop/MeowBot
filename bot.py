43333b80f9d258ccfda72caddf2108e69b1961ea
Natalie97-boop <natalie.c.ly@outlook.com>
2021-01-05 15:11:58 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,28 +19,26 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     guild_name = message.guild.name
-    channel_name = message.channel.name
     if message.author == bot.user:
         return
     if message.content == "MeowCount":
-        f = open((guild_name + " " + channel_name + ".txt"),"r")
-        num = f.readline()
-        num2 = int(num,10) + 1
-        num3 = str(num2)
-        await message.channel.send(num3 + " Meows have been served")
-        f.close()
-        return
+        with open((guild_name + ".txt"),"r") as filehandle:
+            num = filehandle.readline()
+            num2 = int(num) + 1
+            num3 = str(num2)
+            await message.channel.send(num3 + " Meows have been served")
+            return
     if re.search(pattern, message.content):
         try:
-            with open((guild_name + " " + channel_name + ".txt"),"r") as filehandle:
+            with open((guild_name + ".txt"),"r") as filehandle:
             	num = filehandle.readline()
-            num2 = int(num,10) + 1
+            num2 = int(num) + 1
             num3 = str(num2)
             await message.channel.send("Meow :black_cat:")
-            with open((guild_name + " " + channel_name + ".txt"),"w") as filehandle:
+            with open((guild_name + ".txt"),"w") as filehandle:
                 filehandle.write(num3)
         except:
-            with open((guild_name + " " + channel_name + ".txt"),"w") as filehandle:
+            with open((guild_name + ".txt"),"w") as filehandle:
                 filehandle.write("1")
             await message.channel.send("Meow :black_cat:")
             await message.channel.send("First Meow! Woo!")
