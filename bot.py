aa6a6117047e5c94e26e9c2cd3f628e76f50ef08
Natalie97-boop <natalie.c.ly@outlook.com>
2021-01-05 20:12:19 -0500

    Updated witth Nyaa
    added lots of images

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import os
+import random
 import re
 import discord
 from discord.ext import commands
@@ -19,18 +21,27 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     guild_name = message.guild.name
+
     if message.author == bot.user:
         return
+    
+    if message.content == "nyaa":
+        file = random.choice(os.listdir("./Catgirl_pics"))
+        await message.channel.send("OwO")
+        await message.channel.send(file=discord.File("./Catgirl_pics/" + file))
+        
+
     if message.content == "MeowCount":
         try:
-        with open((guild_name + ".txt"),"r") as filehandle:
-            num = filehandle.readline()
-            num2 = int(num) + 1
-            num3 = str(num2)
-            await message.channel.send(num3 + " Meows have been served")
-            return
+            with open((guild_name + ".txt"),"r") as filehandle:
+                num = filehandle.readline()
+                num2 = int(num) + 1
+                num3 = str(num2)
+                await message.channel.send(num3 + " Meows have been served")
+                return
         except:
             await message.channel.send("There have been no Meows in this server")
+
     if re.search(pattern, message.content):
         try:
             with open((guild_name + ".txt"),"r") as filehandle:
@@ -45,4 +56,5 @@ async def on_message(message):
                 filehandle.write("1")
             await message.channel.send("Meow :black_cat:")
             await message.channel.send("First Meow! Woo!")
+            
 bot.run(TOKEN)
